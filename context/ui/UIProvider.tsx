4b804e76e811e52aa62570e25983132a8582f794
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-08-09 09:02:46 +0200

    finished toggle sideBar visibility

--- context/ui/UIProvider.tsx
+++ context/ui/UIProvider.tsx
@@ -16,8 +16,12 @@ interface Props {
 export const UIProvider: FC<Props> = ({ children }) => {
   const [state, dispatch] = useReducer(uiReducer, UI_INITIAL_STATE);
 
+  const openSideMenu = () => dispatch({ type: 'UI-Open Sidebar' });
+
+  const closeSideMenu = () => dispatch({ type: 'UI-Close Sidebar' });
+
   return (
-    <UIContext.Provider value={{ sideMenuOpen: false }}>
+    <UIContext.Provider value={{ ...state, openSideMenu, closeSideMenu }}>
       {children}
     </UIContext.Provider>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,13 +9,16 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 import { darkTheme, lightTheme } from '@/themes';
+import { UIProvider } from '@/context/ui';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
   );
 }
 
