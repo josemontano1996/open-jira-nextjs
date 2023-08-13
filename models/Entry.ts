b9b37c4b58edda4c78c9a5570abb2bb38ba473c4
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-08-13 14:29:41 +0200

    added read entries

--- models/Entry.ts
+++ models/Entry.ts
@@ -1,7 +1,7 @@
 import mongoose, { Model, Schema, mongo } from 'mongoose';
 import { Entry } from '@/interfaces';
 
-interface IEntry extends Entry {}
+export interface IEntry extends Entry {}
 
 const entrySchema = new Schema({
   description: { type: String, required: true },
