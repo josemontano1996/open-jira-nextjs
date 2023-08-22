f9626b4fbf0d71612aa5f0585865793d9fd8436c
Jose Manuel Montano Mengual <josemanuel1996enologia@gmail.com>
2023-08-22 09:20:51 +0200

    added entries/[id]

---
+++ pages/entries/[id]/index.tsx
@@ -0,0 +1,104 @@
+import { useState, ChangeEvent, useMemo } from 'react';
+import {
+  capitalize,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  Radio,
+  RadioGroup,
+  TextField,
+  IconButton,
+} from '@mui/material';
+import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
+import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
+import { Layout } from '@/components/layouts';
+import { EntryStatus } from '@/interfaces';
+
+const validStatus: EntryStatus[] = ['pending', 'in-progress', 'completed'];
+
+const EntryPage = () => {
+  const [inputValue, setInputValue] = useState('');
+  const [status, setStatus] = useState<EntryStatus>('pending');
+  const [touched, setTouched] = useState(false);
+
+  const isNotValid = useMemo(() => inputValue.length <= 0 && touched, [inputValue, touched]);
+
+  const onTextFieldChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
+
+  const onStatusChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setStatus(event.target.value as EntryStatus);
+  };
+
+  const onSave = () => {};
+
+  return (
+    <Layout title='....'>
+      <Grid container justifyContent='center' sx={{ marginTop: 2 }}>
+        <Grid item xs={12} sm={8} md={6}>
+          <Card>
+            <CardHeader title={`Entry: ${inputValue}`} subheader={`Created at `}></CardHeader>
+            <CardContent>
+              <TextField
+                sx={{ marginTop: 2, marginBottom: 1 }}
+                fullWidth
+                placeholder='New Entry'
+                autoFocus
+                multiline
+                label='New entry'
+                value={inputValue}
+                onBlur={() => setTouched(true)}
+                onChange={onTextFieldChange}
+                helperText={isNotValid && 'Write an entry'}
+                error={isNotValid}
+              />
+              <FormControl>
+                <FormLabel>Status: </FormLabel>
+                <RadioGroup row value={status} onChange={onStatusChange}>
+                  {validStatus.map((option) => (
+                    <FormControlLabel
+                      key={option}
+                      value={option}
+                      control={<Radio />}
+                      label={capitalize(option)}
+                    />
+                  ))}
+                </RadioGroup>
+              </FormControl>
+            </CardContent>
+            <CardActions>
+              <Button
+                startIcon={<SaveOutlinedIcon />}
+                variant='contained'
+                fullWidth
+                onClick={onSave}
+                disabled={inputValue.length <= 0}
+              >
+                Save
+              </Button>
+            </CardActions>
+          </Card>
+        </Grid>
+      </Grid>
+      <IconButton
+        sx={{
+          position: 'fixed',
+          bottom: 30,
+          right: 30,
+          backgroundColor: 'error.dark',
+        }}
+      >
+        <DeleteOutlineOutlinedIcon />
+      </IconButton>
+    </Layout>
+  );
+};
+
+export default EntryPage;
